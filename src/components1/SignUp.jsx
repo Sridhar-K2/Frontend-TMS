ca9c5164cb53cc2e49325f5535011b0c95c38002
K.sridhar <142417968+Sridhar-K2@users.noreply.github.com>
2024-06-01 19:42:05 +0530

    first commit

--- src/components1/SignUp.jsx
+++ src/components1/SignUp.jsx
@@ -61,7 +61,7 @@ const SignUp = () => {
                 },
                 {
                     headers: { 'Content-Type': 'application/json' },
-                    withCredentials: true
+                    // withCredentials: true
                 }
             );
 
